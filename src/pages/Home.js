e5fad30b6523912e0786751001a725d6f893bb16
Oliver Eddy <olivereddy.developer@gmail.com>
2024-02-29 17:25:40 +1300

    Add react-router-dom and update dependencies
    Header.js and Home.js content is displaying on the browser, via App.js

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// Import BlogPost component if you plan to render blog posts here
+// import BlogPost from '../components/BlogPost';
+
+function Home() {
+  return (
+    <main>
+      <h2>Welcome to My Blog</h2>
+      <p>This is where blog content will go.</p>
+      {/* Example usage of BlogPost component, uncomment when ready */}
+      {/* <BlogPost title="Blog Post 1" content="This is my first blog post." /> */}
+    </main>
+  );
+}
+
+export default Home;
